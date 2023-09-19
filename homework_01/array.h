05b4ceda8d004cb1e415732f8e12e3f1873f6e71
738NGX <ningyue@738ngx.site>
2023-09-19 16:13:56 +0800

    first commit

---
+++ homework_01/array.h
@@ -0,0 +1,44 @@
+﻿#include <iostream>
+#include <cassert>
+
+namespace Sufe
+{
+  template <class _TYPE, int _SIZE>
+  class Array
+  {
+  public:
+    // TODO: 实现Iterator类
+    class Iterator
+    {
+    };
+
+  public:
+    // 缺省构造函数
+    Array() : m_data{0} {}
+    // TODO: 1) 实现初始化列表的构造函数
+    Array(std::initializer_list<_TYPE> list);
+
+    int Size() const { return _SIZE; }
+
+    // TODO: 2) 实现排序功能
+    void Sort();
+
+    // TODO: 3) 实现begin()和end()
+    Iterator begin();
+    Iterator end();
+
+    // TODO: 4) 实现At()和[]运算符重载
+    _TYPE &At(int idx);
+    _TYPE &operator[](int idx);
+    const _TYPE &operator[](int index) const;
+
+    friend std::ostream &operator<<(std::ostream &out, const Array &arr)
+    {
+      // TODO: 5) 实现<<运算符重载
+      return out;
+    }
+
+  private:
+    _TYPE m_data[_SIZE];
+  };
+}
---
+++ homework_01/main.cpp
@@ -0,0 +1,34 @@
+﻿#include <iostream>
+#include "array.h"
+
+using namespace std;
+
+int main()
+{
+  Sufe::Array<int, 4> arr_x;
+  cout << "arr_x has size " << arr_x.Size() << endl;
+
+  /* TODO: Enable following tests one by one
+  Sufe::Array<int, 4> arr_4 = {3, 2, 1}; // 初始化元素个数 <= 4
+  Sufe::Array<int, 7> arr_7 = {8, 4, 6, 5, 7, 9, 3};
+  for (int i = 0; i < 4; i++)
+  {
+    arr_x.At(i) = 3 - i;
+  }
+
+  cout << "arr_4 has size " << arr_4.Size() << endl;;
+  cout << "arr_7 has size " << arr_7.Size() << endl;
+
+  cout << "initial elements of arr_7 are ";
+  for (auto num : arr_7)
+  {
+    cout << num << " ";
+  }
+  cout << endl;
+
+  // Call Sort and operator<<() overloading
+  arr_7.Sort();
+  cout << "sorted elements of arr_7 are " << arr_7 << endl;
+  */
+  return 0;
+}
