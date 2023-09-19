f0bf6e95b4f1ad17caa1329f4504fce1f4020849
738NGX <ningyue@738ngx.site>
2023-09-19 19:20:47 +0800

    2309191920

--- homework_01/README.md
+++ homework_01/README.md
@@ -1,6 +1,6 @@
 # Homework 01-230919
 
-## 📜 需求
+## 📜 Demands
 
 - 实现`std::array`的大部分功能，包括
     - For-each遍历
@@ -11,3 +11,22 @@
     - for-each遍历
     - 调用Sort()，对数组自身排序
 
+## 📝 Efforts
+
+### Makefile
+
+压缩包中给到的makefile在vscode中进行编译遇到的问题是:
+
+- 不能主动创建build文件夹,会报错如下:
+
+    ```
+    process_begin: CreateProcess(NULL, mkdir build, ...) failed.
+    make (e=2): 系统找不到指定的文件。
+    make: *** [Makefile:24: build] Error 2
+    ```
+
+- 在人工创建build文件后,能正常生成*main.o*和*runme.exe*文件,但是vscode仍然报错生成失败,无法进入到调试界面.
+
+**解决方案:**
+
+最后经过一番搜索和尝试之后,发现可以在每个命令前添加`cmd /C`,强制指定使用cmd而不是默认的powershell来跑编译.现在确实不会再有报错了,不过这么干的后果大概是makefile不能跨平台了(毕竟Linux和macos并没有cmd).
--- homework_01/main.cpp
+++ homework_01/main.cpp
@@ -2,33 +2,36 @@
 #include "array.h"
 
 using namespace std;
+using Sufe::Array;
 
 int main()
 {
-  Sufe::Array<int, 4> arr_x;
-  cout << "arr_x has size " << arr_x.Size() << endl;
+    cout<<"make test"<<endl;
+    /*
+    Sufe::Array<int, 4> arr_x;
+    cout << "arr_x has size " << arr_x.Size() << endl;
+    */
+    /* TODO: Enable following tests one by one
+    Sufe::Array<int, 4> arr_4 = {3, 2, 1}; // 初始化元素个数 <= 4
+    Sufe::Array<int, 7> arr_7 = {8, 4, 6, 5, 7, 9, 3};
+    for (int i = 0; i < 4; i++)
+    {
+        arr_x.At(i) = 3 - i;
+    }
 
-  /* TODO: Enable following tests one by one
-  Sufe::Array<int, 4> arr_4 = {3, 2, 1}; // 初始化元素个数 <= 4
-  Sufe::Array<int, 7> arr_7 = {8, 4, 6, 5, 7, 9, 3};
-  for (int i = 0; i < 4; i++)
-  {
-    arr_x.At(i) = 3 - i;
-  }
+    cout << "arr_4 has size " << arr_4.Size() << endl;;
+    cout << "arr_7 has size " << arr_7.Size() << endl;
 
-  cout << "arr_4 has size " << arr_4.Size() << endl;;
-  cout << "arr_7 has size " << arr_7.Size() << endl;
+    cout << "initial elements of arr_7 are ";
+    for (auto num : arr_7)
+    {
+        cout << num << " ";
+    }
+    cout << endl;
 
-  cout << "initial elements of arr_7 are ";
-  for (auto num : arr_7)
-  {
-    cout << num << " ";
-  }
-  cout << endl;
-
-  // Call Sort and operator<<() overloading
-  arr_7.Sort();
-  cout << "sorted elements of arr_7 are " << arr_7 << endl;
-  */
-  return 0;
+    // Call Sort and operator<<() overloading
+    arr_7.Sort();
+    cout << "sorted elements of arr_7 are " << arr_7 << endl;
+    */
+    return 0;
 }
