4a002a097afca1c9fff20fbac946361f011f61df
738NGX <ningyue@738ngx.site>
2023-11-23 19:10:15 +0800

    2311231910

--- homework_04/heap.cpp
+++ homework_04/heap.cpp
@@ -3,61 +3,61 @@
 
 namespace Sufe
 {
-  // to extract the root which is the minimum element
-  Node *Heap::ExtractMin()
-  {
-    // data[0] is a place holder.
-    if (m_data.size() <= 1)
+    // to extract the root which is the minimum element
+    Node *Heap::ExtractMin()
     {
-      return nullptr;
+        // data[0] is a place holder.
+        if (m_data.size() <= 1)
+        {
+            return nullptr;
+        }
+
+        // Store the minimum value, and remove it from heap
+        Node *root = m_data[1];
+        m_data[1] = m_data.back();
+        m_data.pop_back();
+
+        if (m_data.size() > 2)
+        {
+            MinHeapify(1);
+        }
+
+        return root;
     }
 
-    // Store the minimum value, and remove it from heap
-    Node *root = m_data[1];
-    m_data[1] = m_data.back();
-    m_data.pop_back();
-
-    if (m_data.size() > 2)
+    // Inserts a new key 'k'
+    void Heap::InsertKey(Node *nd)
     {
-      MinHeapify(1);
+        // First insert the new key at the end
+        m_data.push_back(nd);
+        int i = (int)m_data.size() - 1;
+
+        // Fix the min heap property if it is violated
+        while (i > 1 && m_data[Parent(i)]->key > m_data[i]->key)
+        {
+            std::swap(m_data[i], m_data[Parent(i)]);
+            i = Parent(i);
+        }
     }
 
-    return root;
-  }
-
-  // Inserts a new key 'k'
-  void Heap::InsertKey(Node *nd)
-  {
-    // First insert the new key at the end
-    m_data.push_back(nd);
-    int i = (int)m_data.size() - 1;
-
-    // Fix the min heap property if it is violated
-    while (i > 1 && m_data[Parent(i)]->key > m_data[i]->key)
+    void Heap::MinHeapify(int i)
     {
-      std::swap(m_data[i], m_data[Parent(i)]);
-      i = Parent(i);
-    }
-  }
+        int l = Left(i);
+        int r = Right(i);
+        int min = i;
+        if (l < (int)m_data.size() && m_data[l]->key < m_data[min]->key)
+        {
+            min = l;
+        }
+        if (r < (int)m_data.size() && m_data[r]->key < m_data[min]->key)
+        {
+            min = r;
+        }
 
-  void Heap::MinHeapify(int i)
-  {
-    int l = Left(i);
-    int r = Right(i);
-    int min = i;
-    if (l < (int)m_data.size() && m_data[l]->key < m_data[min]->key)
-    {
-      min = l;
+        if (min != i)
+        {
+            std::swap(m_data[i], m_data[min]);
+            MinHeapify(min);
+        }
     }
-    if (r < (int)m_data.size() && m_data[r]->key < m_data[min]->key)
-    {
-      min = r;
-    }
-
-    if (min != i)
-    {
-      std::swap(m_data[i], m_data[min]);
-      MinHeapify(min);
-    }
-  }
 }
--- homework_04/heap.h
+++ homework_04/heap.h
@@ -4,33 +4,33 @@
 
 namespace Sufe
 {
-  class Heap
-  {
-  public:
-    // add NULL element at index 0.
-    Heap() { m_data.push_back(NULL); }
+    class Heap
+    {
+    public:
+        // add NULL element at index 0.
+        Heap() { m_data.push_back(NULL); }
 
-    // to extract the root which is the minimum element
-    Node *ExtractMin();
+        // to extract the root which is the minimum element
+        Node *ExtractMin();
 
-    // Inserts a new key: pointer to node of BinaryTree
-    void InsertKey(Node *nd);
+        // Inserts a new key: pointer to node of BinaryTree
+        void InsertKey(Node *nd);
 
-    bool IsEmpty() { return m_data.size() <= 1; }
-    // Actual size without counting element at index 0
-    int Size() { return m_data.size() - 1; }
+        bool IsEmpty() { return m_data.size() <= 1; }
+        // Actual size without counting element at index 0
+        int Size() { return m_data.size() - 1; }
 
-  private:
-    // A recursive method to heapify a subtree with root at given index
-    // This method assumes that the subtrees are already heapified
-    void MinHeapify(int i);
+    private:
+        // A recursive method to heapify a subtree with root at given index
+        // This method assumes that the subtrees are already heapified
+        void MinHeapify(int i);
 
-    int Parent(int i) { return i / 2; }
-    // to get index of left child of node at index i
-    int Left(int i) { return 2 * i; }
-    // to get index of right child of node at index i
-    int Right(int i) { return (2 * i + 1); }
+        int Parent(int i) { return i / 2; }
+        // to get index of left child of node at index i
+        int Left(int i) { return 2 * i; }
+        // to get index of right child of node at index i
+        int Right(int i) { return (2 * i + 1); }
 
-    std::vector<Node *> m_data;
-  };
+        std::vector<Node *> m_data;
+    };
 }
